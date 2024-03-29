4b494a649431f7d622944d1e57a3cf912fee2884
Turi Souza <turisouza@MacBook-Pro-de-Turi.local>
2024-03-29 12:03:59 -0300

    first commit

---
+++ src/componentes/CampoTexto/index.tsx
@@ -0,0 +1,37 @@
+import "./CampoTexto.css";
+
+interface CampoTextoProps {
+  aoAlterado: (valor: string) => void;
+  placeholder: string;
+  label: string;
+  valor: string;
+  obrigatorio?: boolean;
+}
+
+const CampoTexto = ({
+  aoAlterado,
+  placeholder,
+  label,
+  valor,
+  obrigatorio = false,
+}: CampoTextoProps) => {
+  const placeholderModificada = `${placeholder}...`;
+
+  const aoDigitado = (evento: React.ChangeEvent<HTMLInputElement>) => {
+    aoAlterado(evento.target.value);
+  };
+
+  return (
+    <div className="campo-texto">
+      <label>{label}</label>
+      <input
+        value={valor}
+        onChange={(evento) => aoDigitado(evento)}
+        required={obrigatorio}
+        placeholder={placeholderModificada}
+      />
+    </div>
+  );
+};
+
+export default CampoTexto;
